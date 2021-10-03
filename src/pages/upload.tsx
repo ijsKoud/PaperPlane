230491c063890f41e5f3d8fe81fa3d5c6c4048f2
DaanGamesDG <daan@daangamesdg.wtf>
2021-10-03 21:24:05 +0200

    feat(Upload): adding unauthorized page

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -2,15 +2,28 @@ import type { NextPage } from "next";
 import React from "react";
 import { FileUpload, LinkUpload } from "../components/pages/upload";
 import Head from "next/head";
+import { useAuth } from "../lib/hooks/useAuth";
+import Unauthorized from "../components/pages/errors/401";
+import Loading from "../components/loading";
 
 const Upload: NextPage = () => {
+	const { loading, user } = useAuth();
+
 	return (
 		<main className="upload">
 			<Head>
 				<title>PaperPlane - Upload</title>
 			</Head>
-			<FileUpload />
-			<LinkUpload />
+			{loading ? (
+				<Loading />
+			) : user ? (
+				<>
+					<FileUpload />
+					<LinkUpload />
+				</>
+			) : (
+				<Unauthorized />
+			)}
 		</main>
 	);
 };
