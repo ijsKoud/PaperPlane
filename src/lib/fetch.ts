54d1f3877580fa6962c335114fcde937d4375cfa
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-21 13:23:28 +0100

    feat: add fetch function

---
+++ src/lib/fetch.ts
@@ -0,0 +1,17 @@
+import type { AxiosPromise, AxiosRequestConfig, CancelToken } from "axios";
+import axios from "axios";
+
+export function getCancelToken() {
+	return axios.CancelToken.source();
+}
+
+export function fetch<V = unknown>(path: string, cancelToken?: CancelToken, options: AxiosRequestConfig = {}): AxiosPromise<V> {
+	options.url = `${process.env.NEXT_PUBLIC_DOMAIN}${path}`;
+	options.withCredentials ??= true;
+	options.headers ??= {};
+	options.cancelToken ??= cancelToken;
+
+	if (options.method !== "get" && options.method !== "GET") options.headers["Content-Type"] ??= "application/json";
+
+	return axios(options) as AxiosPromise<V>;
+}
