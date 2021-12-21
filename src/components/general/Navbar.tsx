d1e08269a6751a83eba2eb2d8e362b17f18295b1
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-21 11:32:47 +0100

    feat: add navbar with dropdown

---
+++ src/components/general/Navbar.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Navbar: React.FC = () => {
+	const [dropdownActive, setDropdownActive] = useState(false);
+
+	const closeDropdown = () => setDropdownActive(false);
+	const toggleDropdown = () => setDropdownActive(!dropdownActive);
+
+	return (
+		<nav className="navbar">
+			<div className="navbar-logo">
+				<Image src="/assets/svg/paperplane.svg" alt="Logo" width={70} height={70} />
+				<h1>PaperPlane</h1>
+			</div>
+			<div className="navbar-user">
+				<button onClick={toggleDropdown}>
+					<i className="fas fa-user" /> DaanGamesDG
+				</button>
+				<ul className={dropdownActive ? "navbar-dropdown active" : "navbar-dropdown"}>
+					<NavLink closeDropdown={closeDropdown} name="Home" icon="fas fa-home" path="/dashboard" />
+					<NavLink closeDropdown={closeDropdown} name="Upload" icon="fas fa-upload" path="/upload" />
+					<NavLink closeDropdown={closeDropdown} name="Settings" icon="fas fa-cog" path="/settings" />
+					<li className="navbar-dropdown-item">
+						<Link href="/dashboard">
+							<a onClick={closeDropdown}>
+								<i className="fas fa-sign-out-alt" /> Logout
+							</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+interface Props {
+	closeDropdown: () => void;
+	path: string;
+	name: string;
+	icon: string;
+}
+
+const NavLink: React.FC<Props> = ({ closeDropdown, path, name, icon }) => (
+	<li className="navbar-dropdown-item">
+		<Link href={path}>
+			<a onClick={closeDropdown}>
+				<i className={icon} /> {name}
+			</a>
+		</Link>
+	</li>
+);
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/index.scss";
 import type { AppProps } from "next/app";
 
+import Navbar from "../components/general/Navbar";
+
 const App = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Navbar />
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default App;
