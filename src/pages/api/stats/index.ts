536dc23277e585bbf0d646d5cc1a431e06e0d79f
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-21 16:48:47 +0100

    fix(Stats): showing incorrect amount of files

--- src/pages/api/stats/index.ts
+++ src/pages/api/stats/index.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { readdir } from "fs/promises";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { DATA_DIR, Stats } from "../../../lib";
+import { DATA_DIR, FILE_DATA_DIR, Stats } from "../../../lib";
 import { formatBytes, sizeOfDir } from "../../../lib/utils";
 
 const client = new PrismaClient();
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	const rawBytes = await sizeOfDir(DATA_DIR);
 	const bytes = formatBytes(rawBytes);
 
-	const files = await readdir(DATA_DIR);
+	const files = await readdir(FILE_DATA_DIR);
 	const links = await client.url.findMany();
 
 	res.json({
