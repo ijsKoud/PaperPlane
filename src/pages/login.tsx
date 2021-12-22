5bc6e16b0d20bf6530037e93ad4646242c6f097c
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-22 21:32:33 +0100

    feat(Login): add logic for authentication

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,46 @@
 import type { NextPage } from "next";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import { useAuth } from "../lib/hooks/useAuth";
 import { useRouter } from "next/router";
-import type { LoginCreds } from "../lib";
+import { ApiError, fetch, LoginCreds } from "../lib";
 
 import CredentialForm from "../components/login/CredentialForm";
+import type { AxiosError } from "axios";
+import { alert } from "../lib/notifications";
 
 const Dashboard: NextPage = () => {
-	const { user, fetch } = useAuth();
+	const [userData, setUserData] = useState<LoginCreds | null>(null);
+	const { user, fetch: userFetch } = useAuth();
 	const router = useRouter();
 
 	useEffect(() => {
 		if (user) void router.replace("/dashboard");
 	}, [user]);
 
-	const onSubmit = (data: LoginCreds) => {
-		void 0;
-		fetch();
+	const onSubmit = async (data: LoginCreds) => {
+		const has2faEnabled = await fetch<{ enabled: boolean }>("/api/auth/2fa", undefined, {
+			method: "POST",
+			data: { username: data.username }
+		}).catch(() => ({ data: { enabled: false } }));
+		if (has2faEnabled.data.enabled) {
+			setUserData(data);
+			return;
+		}
+
+		try {
+			const token = await fetch<{ token: string }>("/api/auth/login", undefined, {
+				method: "POST",
+				data
+			});
+			localStorage.setItem("PAPERPLANE_AUTH", token.data.token);
+			userFetch();
+		} catch (error) {
+			if (!error || typeof error !== "object" || !("isAxiosError" in error)) return;
+
+			const err = error as AxiosError<ApiError>;
+			alert("Error while logging in", `${err.response?.data.message ?? "Unknown error, please try again later!"}`);
+		}
 	};
 
 	return (
