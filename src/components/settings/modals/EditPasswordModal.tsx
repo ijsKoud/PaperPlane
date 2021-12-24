ec8fce0a8362bae0b962b106c9c9e71e86a9f6ad
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-24 13:02:21 +0100

    feat: add editpassword settings modal

---
+++ src/components/settings/modals/EditPasswordModal.tsx
@@ -0,0 +1,63 @@
+import { Formik, Form, Field } from "formik";
+import React, { useState } from "react";
+import { fetch } from "../../../lib/fetch";
+import { useAuth } from "../../../lib/hooks/useAuth";
+import { object, string } from "yup";
+import PulseLoader from "../../general/PulseLoader";
+
+interface Props {
+	handleClose: () => void;
+}
+
+const EditPasswordModal: React.FC<Props> = ({ handleClose }) => {
+	const { fetch: FetchUser } = useAuth();
+	const [show, setShow] = useState(false);
+
+	const validationSchema = object({
+		password: string().min(5, "Password is too short").required("Password is required")
+	});
+
+	const submit = async (data: { password: string }) => {
+		await fetch("/api/user", undefined, { method: "PATCH", data: { password: data.password } });
+		FetchUser();
+		handleClose();
+	};
+
+	return (
+		<div className="edit-wrapper">
+			<i className="fas fa-times" onClick={handleClose} />
+			<Formik onSubmit={submit} initialValues={{ password: "" }} validationSchema={validationSchema} validateOnMount validateOnChange>
+				{({ isValid, isSubmitting, errors }) => (
+					<Form className="edit-container">
+						{isSubmitting ? (
+							<PulseLoader />
+						) : (
+							<>
+								<p>New password</p>
+								<div className="settings-password">
+									<Field
+										as="input"
+										type={show ? "text" : "password"}
+										id="password"
+										name="password"
+										placeholder="password..."
+										style={{ width: "90%" }}
+									/>
+									<button className="settings-button" onClick={() => setShow(!show)}>
+										<i className={show ? "fas fa-eye-slash" : "fas fa-eye"} />
+									</button>
+								</div>
+								<span>{errors.password}</span>
+								<button disabled={!isValid} type="submit">
+									<i className="fas fa-save" /> Update
+								</button>
+							</>
+						)}
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
+};
+
+export default EditPasswordModal;
