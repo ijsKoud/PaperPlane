3c57d76bb3d8bf81b85f892c2b1a0050efe2bfa8
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-26 20:36:38 +0100

    fix(Lib/Settings): fileNameSize returns Infinity instead of number

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,7 +1,9 @@
 const settings = {
 	allowedExtensions: parseEnvArray(process.env.ALLOWED_EXTENSIONS),
 	maxFilesPerRequest: parseEnvInt(process.env.MAX_FILES_PER_REQUEST),
-	maxFileSize: parseEnvInt(process.env.MAX_FILE_SIZE)
+	maxFileSize: parseEnvInt(process.env.MAX_FILE_SIZE),
+	customFileName: parseEnvBoolean(process.env.CUSTOM_FILE_NAME),
+	fileNameLength: parseEnvInt(process.env.FILE_NAME_LENGTH, 8)
 };
 
 function parseEnvArray(item: string | undefined): string[] {
@@ -10,11 +12,18 @@ function parseEnvArray(item: string | undefined): string[] {
 	return item.split(",");
 }
 
-function parseEnvInt(item: string | undefined): number {
-	if (!item) return Infinity;
+function parseEnvInt(item: string | undefined, fallback?: number): number {
+	if (!item) return fallback ?? Infinity;
 
 	const parsed = Number(item);
-	return isNaN(parsed) ? Infinity : parsed;
+	return isNaN(parsed) ? fallback ?? Infinity : parsed;
+}
+
+function parseEnvBoolean(item: string | undefined): boolean {
+	if (!item) return false;
+
+	if (item.toLowerCase() === "false") return false;
+	return true;
 }
 
 export default settings;
