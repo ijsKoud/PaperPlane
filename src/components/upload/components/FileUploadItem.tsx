2c11cd33c2e03cc3426d06111fd8a9cbed95098e
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-28 12:09:26 +0100

    feat(FileUploadItem): add error handling

--- src/components/upload/components/FileUploadItem.tsx
+++ src/components/upload/components/FileUploadItem.tsx
@@ -1,6 +1,8 @@
+import type { AxiosError } from "axios";
 import React, { useEffect, useState } from "react";
-import { fetch, getCancelToken } from "../../../lib";
+import { ApiError, fetch, getCancelToken } from "../../../lib";
 import { useAuth } from "../../../lib/hooks/useAuth";
+import { alert } from "../../../lib/notifications";
 import ToolTip from "../../general/ToolTip";
 
 interface Props {
@@ -13,17 +15,31 @@ const FileUploadItem: React.FC<Props> = ({ file, index }) => {
 	const [progress, setProgress] = useState(0);
 	const { cancel, token } = getCancelToken();
 
-	useEffect(() => {
+	const func = async () => {
 		const data = new FormData();
 		data.append("upload", file);
-		fetch("/api/upload", token, {
-			method: "POST",
-			data,
-			headers: { Authorization: user?.token ?? "", "Content-Type": "multipart/form-data" },
-			onUploadProgress: (ev) => setProgress(Math.round((100 * ev.loaded) / ev.total))
-		}).catch(() => void 0);
 
-		return () => cancel();
+		try {
+			await fetch("/api/upload", token, {
+				method: "POST",
+				data,
+				cancelToken: token,
+				headers: { Authorization: user?.token ?? "", "Content-Type": "multipart/form-data" },
+				onUploadProgress: (ev) => setProgress(Math.round((100 * ev.loaded) / ev.total))
+			});
+		} catch (error) {
+			if (!error || typeof error !== "object") return;
+			if (error instanceof Error && error.message === "cancelled") return;
+			if (!("isAxiosError" in error)) return;
+
+			const err = error as AxiosError<ApiError>;
+			alert("Something went wrong while uploading a file", err.response?.data.message ?? "Unknown error, please try again later.");
+		}
+	};
+
+	useEffect(() => {
+		void func();
+		return () => cancel("cancelled");
 	}, []);
 
 	return (
