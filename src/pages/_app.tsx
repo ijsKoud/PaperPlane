e6b3c58070487ce8a289255e0c33f10c6126b0d5
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-05 12:30:33 +0200

    fix: dep incompatibilty

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import "react-notifications-component/dist/theme.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/general/Navbar";
 import { AnimatePresence, motion } from "framer-motion";
-import ReactNotificationsComponent from "react-notifications-component";
+import { ReactNotifications } from "react-notifications-component";
 import { defaultVariant } from "../lib/clientConstants";
 import { ProvideAuth } from "../lib/hooks/useAuth";
 
@@ -21,7 +21,7 @@ const variants = {
 const App = ({ Component, pageProps, router }: AppProps) => {
 	return (
 		<ProvideAuth>
-			<ReactNotificationsComponent />
+			<ReactNotifications />
 			<Navbar />
 			<AnimatePresence exitBeforeEnter>
 				<motion.div key={router.route} variants={variants} initial="initial" animate="animate" exit="exit">
