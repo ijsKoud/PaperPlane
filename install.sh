3255f9b4cf230afbbcd5cdaefed37ce7e19129a9
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-18 21:48:19 +0200

    chore: add install bash script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo 'Installing PaperPlane...'
+
+# Variables
+DIRECTORY='~/paperplane'
+COMMAND="docker run --name=paperplane -d -v ~/paperplane:/paperplane/data -p 3000:3000 daangamesdg/paperplane"
+
+# Script
+echo "Creating a shared data directory: ${DIRECTORY}"
+mkdir -p $DIRECTORY
+chown 1639:1639 $DIRECTORY
+
+echo 'Pulling latest image from Docker'
+# TODO: update image to docker
+
+echo 'Running Docker container'
+if [[ -n "$1" ]]; then
+	COMMAND="docker run --name=paperplane -d -v ~/paperplane:/paperplane/data -p 3000:$1 daangamesdg/paperplane"
+fi
+
+eval $COMMAND
+
+echo "Installation complete, PaperPlane is now ready to be used!\n\nIf you run into a situation where the container is shutdown use the following command: '$COMMAND'"
+exit 0
