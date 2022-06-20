44838c6887792c23470971409fb44f2432f14c35
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-20 15:52:21 +0200

    feat(Routes): add password check

--- src/server/components/routes.ts
+++ src/server/components/routes.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from "express";
+import { scryptSync, timingSafeEqual } from "node:crypto";
 import type { Server } from "../Server";
 
 export class Routes {
@@ -11,13 +12,28 @@ export class Routes {
 	}
 
 	private async getFile(req: Request, res: Response) {
-		const { id } = req.params;
+		const { id, password, check } = req.params;
+		const isUserAgent = req.headers["user-agent"] === this.DISCORD_IMAGE_AGENT;
 
 		const user = await this.server.prisma.user.findFirst();
-		if (user?.embedEnabled && req.headers["user-agent"] === this.DISCORD_IMAGE_AGENT) return this.server.next.render(req, res, `/files/${id}`);
-
 		const file = await this.server.prisma.file.findUnique({ where: { id } });
+
 		if (!file) return this.server.next.render404(req, res);
+		if (file.password && !password && !isUserAgent) return this.server.next.render(req, res, `/files/${id}?type=password`);
+		if (user?.embedEnabled && isUserAgent)
+			return this.server.next.render(req, res, `/files/${id}?type=discord&p=${encodeURIComponent(password)}`);
+
+		if (file.password) {
+			const [salt, key] = file.password.split(":");
+			const passwordBuffer = scryptSync(password, salt, 64);
+
+			const keyBuffer = Buffer.from(key, "hex");
+			const match = timingSafeEqual(passwordBuffer, keyBuffer);
+			if (!match) return res.status(404).send({ message: "Incorrect password provided" });
+
+			// check if a check param is present -> send 204 success res back
+			if (check) return res.sendStatus(204);
+		}
 
 		res.sendFile(file.path, (err) => (err ? res.end() : null));
 	}
