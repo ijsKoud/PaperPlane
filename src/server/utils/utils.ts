d173f3fe061501dd4b1a38e5b87f2f518a5515bc
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-21 19:40:47 +0200

    feat: add default user creation

--- src/server/utils/utils.ts
+++ src/server/utils/utils.ts
@@ -1,4 +1,4 @@
-import { randomBytes } from "node:crypto";
+import { randomBytes, scryptSync } from "node:crypto";
 import ShortUniqueId from "short-unique-id";
 import type { Config, NameType } from "./types";
 
@@ -77,4 +77,23 @@ export function formatBytes(bytes: number) {
 	}
 
 	return `${bytes.toFixed(1)} ${units[num]}`;
+}
+
+export function randomChars(length: number) {
+	const charset = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+
+	let res = "";
+	for (let i = 0; i !== length; ++i) res += charset[Math.floor(Math.random() * charset.length)];
+	return res;
+}
+
+export function createToken() {
+	return `${randomChars(24)}.${Buffer.from(Date.now().toString()).toString("base64url")}`;
+}
+
+export function encryptPassword(password: string): string {
+	const salt = randomBytes(16).toString("hex");
+	const pwd = scryptSync(password, salt, 64).toString("hex");
+
+	return `${salt}:${pwd}`;
 }
