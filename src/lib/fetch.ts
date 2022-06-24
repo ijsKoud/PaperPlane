465e6e31f8f0ca3eb89378303c840d684aa6f970
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-24 14:31:31 +0200

    feat: add useAuth hook

---
+++ src/lib/fetch.ts
@@ -0,0 +1,18 @@
+import type { AxiosPromise, AxiosRequestConfig, CancelToken } from "axios";
+import axios from "axios";
+
+export function getCancelToken() {
+	return axios.CancelToken.source();
+}
+
+export function fetch<V = unknown>(path: string, cancelToken?: CancelToken, options: AxiosRequestConfig = {}): AxiosPromise<V> {
+	options.url = path;
+	options.cancelToken ??= cancelToken;
+
+	options.headers ??= {};
+	options.headers["Authorization"] ??= `Bearer ${localStorage.getItem("PAPERPLANE_AUTH")}`;
+
+	if (options.method !== "get" && options.method !== "GET") options.headers["Content-Type"] ??= "application/json";
+
+	return axios(options) as AxiosPromise<V>;
+}
