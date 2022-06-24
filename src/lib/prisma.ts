cc3f2d0da4f0ac61e97e5cb1d60e8cb4017ae84a
DaanGamesDG <daan@daangamesdg.xyz>
2022-06-24 16:08:14 +0200

    perf: use global.prisma for dev runs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-void prisma.$connect();
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") prisma = new PrismaClient();
+else {
+	if (!global.prisma) global.prisma = new PrismaClient();
+	prisma = global.prisma;
+}
 
 export default prisma;
