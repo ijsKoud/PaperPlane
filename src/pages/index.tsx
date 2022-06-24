7fb06f2243847afbe6f4b32043cb189afdd7fb50
ijsKoud <github@ijskoud.dev>
2022-06-24 08:14:50 +0200

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,13 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
 	return (
 		<main>
-			<h1>Template</h1>
+			<div>
+				<div>
+					<img alt="PaperPlane logo" src="assets/svg/paperplane_nobg.svg" />
+					<h1>PAPERPLANE</h1>
+				</div>
+				<p>File uploading. URL Shortening. Protected views.</p>
+			</div>
 		</main>
 	);
 };
