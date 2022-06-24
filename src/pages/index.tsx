1bd4c6265d3e813186a2134f6738b9b3e3f76a6a
ijsKoud <github@ijskoud.dev>
2022-06-24 08:26:09 +0200

    feat(Home): add styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
 	return (
-		<main>
-			<div>
-				<div>
+		<main className="home-page-container">
+			<div className="home-content">
+				<div className="home-title">
 					<img alt="PaperPlane logo" src="assets/svg/paperplane_nobg.svg" />
 					<h1>PAPERPLANE</h1>
 				</div>
