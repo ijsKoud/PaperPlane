c282e979fe5f0dc9093fdb358689826391f1796e
ijsKoud <github@ijskoud.dev>
2022-06-26 18:47:43 +0200

    fix(ModalItem): unused imports

--- src/components/general/Modal/ModalItem.tsx
+++ src/components/general/Modal/ModalItem.tsx
@@ -1,7 +1,6 @@
 import { motion, Variants } from "framer-motion";
 import React from "react";
 import type { FC } from "../../../lib/types";
-import Button from "../Button";
 import BackDrop from "./BackDrop";
 
 const variants: Variants = {
