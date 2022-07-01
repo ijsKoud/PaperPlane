d3ddd5e33c38f1da372163e2f91cdfa3dcbb3979
ijsKoud <github@ijskoud.dev>
2022-07-01 22:01:57 +0200

    fix(Stats): prisma not defined

--- src/pages/api/dashboard/stats.ts
+++ src/pages/api/dashboard/stats.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { formatBytes, getUser } from "../../../lib/utils";
+import prisma from "../../../lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const user = await getUser(req);
