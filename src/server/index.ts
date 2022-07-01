b14bd5942d9368984f83a39405216adfc938c295
ijsKoud <github@ijskoud.dev>
2022-07-01 21:43:00 +0200

    feat: push database updates to db file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,13 @@
 import { config } from "dotenv";
+import { exec } from "node:child_process";
 import { join } from "node:path";
 config({ path: join(process.cwd(), "data", ".env") });
 
 import { Server } from "./Server";
 
-const server = new Server();
-void server.run();
+void (async () => {
+	await new Promise((res) => exec("yarn prisma db push", res));
+
+	const server = new Server();
+	await server.run();
+})();
