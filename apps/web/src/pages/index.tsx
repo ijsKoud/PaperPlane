135c177e6640ed496cff0abe7449ac9d17d769d2
ijsKoud <github@ijskoud.dev>
2022-12-25 14:46:14 +0100

    feat(Web): make landing responsive for mobile

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -7,11 +7,11 @@ export default function Web() {
 			<div className="flex flex-col gap-y-3 items-center justify-center">
 				<div>
 					<div className="flex gap-4">
-						<Logo width={45} height={45} /> <h1 className="text-4xl">PAPERPLANE</h1>
+						<Logo width={45} height={45} /> <h1 className="text-4xl max-sm:text-2xl">PAPERPLANE</h1>
 					</div>
-					<h2 className="text-base">File uploading. URL Shortening. Protected views.</h2>
+					<h2 className="text-base max-sm:text-comment max-sm:font-medium">File uploading. URL Shortening. Protected views.</h2>
 				</div>
-				<div className="flex gap-2">
+				<div className="flex gap-2 max-sm:flex-col max-sm:w-full text-center">
 					<PrimaryButton type="link" href="https://github.com/ijsKoud/paperplane">
 						GitHub
 					</PrimaryButton>
