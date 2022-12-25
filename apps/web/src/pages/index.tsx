855003a44259416fa2bc01560bfd2497915f0898
ijsKoud <github@ijskoud.dev>
2022-12-25 13:26:12 +0100

    feat(Buttons): add secondary button

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { PrimaryButton } from "@paperplane/buttons";
+import { PrimaryButton, SecondaryButton } from "@paperplane/buttons";
 
 export default function Web() {
 	return (
 		<div className="dark:bg-bg-dark">
 			<h1>Web</h1>
 			<PrimaryButton type="button">Hello World</PrimaryButton>
+			<SecondaryButton type="button">Hello World</SecondaryButton>
 		</div>
 	);
 }
