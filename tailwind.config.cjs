125bd3e1081662bbd0132fb9117ad588412fcff0
ijsKoud <github@ijskoud.dev>
2022-12-25 14:10:35 +0100

    fix: css bug fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
 				xl: ["28.13px", { fontWeight: 600 }],
 				"2xl": ["35.16px", { fontWeight: 600 }],
 				"3xl": ["43.95px", { fontWeight: 600 }],
-				"4xl": ["54.9x", { fontWeight: 700, lineHeight: "50px" }]
+				"4xl": ["54.9px", { fontWeight: 700, lineHeight: "50px" }]
 			},
 			colors: {
 				// main
@@ -29,16 +29,16 @@ module.exports = {
 				main: "rgba(36,37,38,1)",
 
 				// white
-				white: "rgba(225, 225, 225, 1)",
-				"white-900": "rgba(225, 225, 225, .9)",
-				"white-800": "rgba(225, 225, 225, .8)",
-				"white-700": "rgba(225, 225, 225, .7)",
-				"white-600": "rgba(225, 225, 225, .6)",
-				"white-500": "rgba(225, 225, 225, .5)",
-				"white-400": "rgba(225, 225, 225, .4)",
-				"white-300": "rgba(225, 225, 225, .3)",
-				"white-200": "rgba(225, 225, 225, .2)",
-				"white-100": "rgba(225, 225, 225, .1)",
+				white: "rgba(255, 255, 255, 1)",
+				"white-900": "rgba(255, 255, 255, .9)",
+				"white-800": "rgba(255, 255, 255, .8)",
+				"white-700": "rgba(255, 255, 255, .7)",
+				"white-600": "rgba(255, 255, 255, .6)",
+				"white-500": "rgba(255, 255, 255, .5)",
+				"white-400": "rgba(255, 255, 255, .4)",
+				"white-300": "rgba(255, 255, 255, .3)",
+				"white-200": "rgba(255, 255, 255, .2)",
+				"white-100": "rgba(255, 255, 255, .1)",
 
 				// bg
 				"bg-dark": "#1F2021",
