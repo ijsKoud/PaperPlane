5cbb80d5378961d82858be54ab35582ac9518347
ijsKoud <github@ijskoud.dev>
2022-12-27 12:41:35 +0100

    feat(SelectMenuComponent): fix transparent bg when absolute

--- packages/forms/src/Select/custom/SelectMenuComponent.tsx
+++ packages/forms/src/Select/custom/SelectMenuComponent.tsx
@@ -5,7 +5,7 @@ export const SelectMenuComponent: React.FC<MenuProps> = (props) => {
 	const { children, innerRef, innerProps } = props;
 	return (
 		<div
-			className="top-full relative overflow-x-hidden overflow-y-auto w-full z-[1] bg-highlight-200 rounded-xl shadow-md my-2 box-border border border-highlight-600"
+			className="top-full absolute overflow-x-hidden overflow-y-auto w-full z-10 bg-[#292a36] rounded-xl shadow-md my-2 box-border border border-highlight-600"
 			ref={innerRef}
 			{...innerProps}
 		>
