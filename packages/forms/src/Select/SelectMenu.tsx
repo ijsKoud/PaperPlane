77ae36d63eabf4eaa7c98f62b97025a5f81a683e
ijsKoud <github@ijskoud.dev>
2022-12-28 12:39:59 +0100

    feat(SelectMenu): add correct props

--- packages/forms/src/Select/SelectMenu.tsx
+++ packages/forms/src/Select/SelectMenu.tsx
@@ -1,22 +1,11 @@
 import type React from "react";
-import ReactSelect from "react-select";
+import ReactSelect, { Props } from "react-select";
 import { SelectInput, SelectControl, SelectMenuComponent, SelectOption, SelectDropdownIndicator, SelectSingleValue } from "./custom";
 
-export const SelectMenu: React.FC = () => {
+export const SelectMenu: React.FC<Props> = (props) => {
 	return (
 		<ReactSelect
-			options={[
-				{ label: "hello", value: "world" },
-				{ label: "hello1", value: "world1" },
-				{ label: "hello2", value: "world2" },
-				{ label: "hello3", value: "world3" },
-				{ label: "hello4", value: "world4" },
-				{ label: "hello5", value: "world5" },
-				{ label: "hello6", value: "world6" },
-				{ label: "hello7", value: "world7" },
-				{ label: "hello8", value: "world8" },
-				{ label: "hello9", value: "world9" }
-			]}
+			{...props}
 			components={{
 				Input: SelectInput,
 				Control: SelectControl,
