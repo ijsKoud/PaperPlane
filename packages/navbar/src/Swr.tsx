6654e36b4f276770bb39c48ea7eacc84d348f260
ijsKoud <github@ijskoud.dev>
2023-01-04 00:06:17 +0100

    feat(Swr): make caching a lot better

--- packages/navbar/src/Swr.tsx
+++ packages/navbar/src/Swr.tsx
@@ -1,20 +1,26 @@
 import type React from "react";
-import { SWRConfig } from "swr";
+import { useState } from "react";
+import { SWRConfig, Cache } from "swr";
 
 const Swr: React.FC<React.PropsWithChildren> = ({ children }) => {
-	const localStorageProvider = (): Map<string, any> => {
+	const [cache] = useState(new Map<string, any>());
+
+	const localStorageProvider = (): Cache<any> => {
+		const parse = (data: string) => (data === "undefined" ? undefined : JSON.parse(data));
+
 		if (typeof localStorage !== "undefined") {
-			const map = new Map<string, any>(JSON.parse(localStorage.getItem("app-cache") || "[]"));
+			for (const [key, data] of Object.entries(localStorage)) {
+				if (key.startsWith("swr-")) cache.set(key.slice(4), parse(data));
+			}
 
 			window.addEventListener("beforeunload", () => {
-				const appCache = JSON.stringify(Array.from(map.entries()));
-				localStorage.setItem("app-cache", appCache);
+				for (const [key, data] of cache.entries()) {
+					localStorage.setItem(`swr-${key}`, JSON.stringify(data));
+				}
 			});
-
-			return map;
 		}
 
-		return new Map<string, any>();
+		return cache;
 	};
 
 	return <SWRConfig value={{ provider: localStorageProvider }}>{children}</SWRConfig>;
