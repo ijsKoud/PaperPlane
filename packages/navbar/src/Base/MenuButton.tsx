60c6952dada68894fddeeb5a046db7a9ad99f456
ijsKoud <github@ijskoud.dev>
2023-01-04 11:06:05 +0100

    fix(MenuButton): deformed cross icon

--- packages/navbar/src/Base/MenuButton.tsx
+++ packages/navbar/src/Base/MenuButton.tsx
@@ -16,9 +16,9 @@ export const MenuButton: React.FC<Props> = ({ openChangelog }) => {
 		<>
 			<MobileMenu active={active} closeMenu={closeMenu} openChangelog={openChangelog} />
 			<button onClick={onClick} className="relative cursor-pointer w-6 h-8 z-20">
-				<div className={`w-6 h-[2px] transition-all absolute right-0 bg-white -translate-y-1 ${active && "rotate-45 translate-y-[1px]"}`} />
+				<div className={`w-6 h-[2px] transition-all absolute right-0 bg-white -translate-y-1 ${active && "rotate-45 !translate-y-[1px]"}`} />
 				<div
-					className={`w-4 h-[2px] transition-all absolute right-0 bg-white translate-y-1 ${active && "-rotate-45 translate-y-[1px] !w-6"}`}
+					className={`w-4 h-[2px] transition-all absolute right-0 bg-white translate-y-1 ${active && "-rotate-45 !translate-y-[1px] !w-6"}`}
 				/>
 			</button>
 		</>
