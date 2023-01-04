d5f89fd6a7d287ef93a5cd3bc7cfe504ec278809
ijsKoud <github@ijskoud.dev>
2023-01-04 15:13:25 +0100

    refactor: add Dashboard Stats to @paperplane/ui

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -2,7 +2,7 @@ import { DashboardNavbar } from "@paperplane/navbar";
 import { getCircleColor } from "@paperplane/utils";
 import type { NextPage } from "next";
 import { Circle } from "rc-progress";
-import { Table, TableEntry } from "@paperplane/ui";
+import { DashboardStatistics, Table, TableEntry } from "@paperplane/ui";
 
 const Dashboard: NextPage = () => {
 	const storagePercentage = 10;
@@ -29,27 +29,7 @@ const Dashboard: NextPage = () => {
 								<strong>7.7</strong> GB out of <strong>10.0</strong> GB used
 							</p>
 						</div>
-						<div className="bg-main p-8 rounded-xl h-full">
-							<h1 className="text-xl">Statistics</h1>
-							<div className="flex flex-wrap gap-x-16 gap-y-4 mt-4">
-								<div>
-									<h2 className="text-lg">Files</h2>
-									<p className="text-4xl">1528</p>
-								</div>
-								<div>
-									<h2 className="text-lg">Shorturls</h2>
-									<p className="text-4xl">38</p>
-								</div>
-								<div>
-									<h2 className="text-lg">Total</h2>
-									<p className="text-4xl">1566</p>
-								</div>
-								<div>
-									<h2 className="text-lg">Version</h2>
-									<p className="text-4xl">4.0.0</p>
-								</div>
-							</div>
-						</div>
+						<DashboardStatistics files={1528} shorturls={38} />
 					</div>
 					<div className="max-w-[calc(1040px)] w-full px-2">
 						<div className="w-full rounded-lg bg-main p-8 flex flex-col gap-2">
