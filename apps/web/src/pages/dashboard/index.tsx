29468fc08463cbb212042f9b8cc9d55e85ec854b
ijsKoud <github@ijskoud.dev>
2023-01-05 01:47:31 +0100

    fix(Ui): max-width issue

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -4,11 +4,11 @@ import { DashboardLayout, DashboardStatistics, DashboardStorageUsage, Table, Tab
 const Dashboard: NextPage = () => {
 	return (
 		<DashboardLayout>
-			<div className="max-w-[1040px] w-full h-80 flex gap-8 items-center px-2 max-md:flex-col max-md:h-auto">
+			<div className="w-full h-80 flex gap-8 items-center px-2 max-md:flex-col max-md:h-auto">
 				<DashboardStorageUsage used={7.7e9} total={1e10} />
 				<DashboardStatistics files={1528} shorturls={38} />
 			</div>
-			<div className="max-w-[calc(1040px)] w-full px-2">
+			<div className="w-full px-2">
 				<div className="w-full rounded-lg bg-main p-8 flex flex-col gap-2">
 					<h1 className="text-xl">Audit Logs</h1>
 					<div className="w-full overflow-x-auto">
--- packages/ui/src/dashboard/Layout.tsx
+++ packages/ui/src/dashboard/Layout.tsx
@@ -5,8 +5,8 @@ export const DashboardLayout: React.FC<React.PropsWithChildren> = ({ children })
 	return (
 		<>
 			<DashboardNavbar />
-			<div className="pt-24">
-				<div className="pt-24 flex flex-col justify-center items-center gap-y-8 max-md:pt-8">{children}</div>
+			<div className="pt-24 grid place-items-center">
+				<div className="pt-24 flex flex-col justify-center items-center gap-y-8 max-md:pt-8 max-w-[1040px] w-full">{children}</div>
 			</div>
 		</>
 	);
