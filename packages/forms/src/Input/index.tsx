a3a67fdba0e6391a5280785a2c8a7271a01f08bf
ijsKoud <github@ijskoud.dev>
2023-01-05 02:04:18 +0100

    feat(Forms): add styles to Input

--- packages/forms/src/Input/index.tsx
+++ packages/forms/src/Input/index.tsx
@@ -1,10 +1,21 @@
 import type React from "react";
+import { getCleanProps, Styles } from "../";
 
-export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => {
+interface InputProps {
+	type: keyof typeof Styles;
+}
+
+type Props = React.InputHTMLAttributes<HTMLInputElement> & InputProps;
+
+export const Input: React.FC<Props> = (props) => {
+	const style = Styles[props.type];
+
 	return (
-		<input
-			className="bg-highlight-200 border border-highlight-600 rounded-xl px-4 py-2 outline-2 outline-transparent outline focus:outline-highlight transition-all placeholder:text-white-600"
-			{...props}
-		/>
+		<>
+			<input
+				className={`${props.className} ${style} border rounded-xl px-4 py-2 outline-2 outline-transparent outline transition-all placeholder:text-white-600`}
+				{...getCleanProps(props)}
+			/>
+		</>
 	);
 };
