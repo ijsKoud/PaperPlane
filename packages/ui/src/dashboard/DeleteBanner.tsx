5967d2a39407d239145f4271116022a27d1e10e9
ijsKoud <github@ijskoud.dev>
2023-01-09 18:16:31 +0100

    feat(DeleteBanner): add enter and exit animation

--- packages/ui/src/dashboard/DeleteBanner.tsx
+++ packages/ui/src/dashboard/DeleteBanner.tsx
@@ -1,5 +1,33 @@
 import { DangerButton, SuccessButton } from "@paperplane/buttons";
 import type React from "react";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+
+const variants = {
+	initial: {
+		transform: "translateY(10px)",
+		opacity: 0,
+		transition: {
+			duration: 0.3,
+			ease: [0.4, 0, 0.2, 1]
+		}
+	},
+	animate: {
+		transform: "translateY(0px)",
+		opacity: 1,
+		transition: {
+			duration: 0.3,
+			ease: [0.4, 0, 0.2, 1]
+		}
+	},
+	exit: {
+		transform: "translateY(-10px)",
+		opacity: 0,
+		transition: {
+			duration: 0.3,
+			ease: [0.4, 0, 0.2, 1]
+		}
+	}
+} satisfies Variants;
 
 interface Props {
 	items: string[];
@@ -10,24 +38,35 @@ export const DashboardDeleteBanner: React.FC<Props> = ({ items, type }) => {
 	const correctTypeForm = items.length === 1 ? type : `${type}s`;
 
 	return (
-		<div className="px-4 fixed bottom-4 max-w-[1008px] w-full">
-			<div className="bg-main p-4 border-white-100 border rounded-xl flex items-center justify-between w-full max-md:flex-col gap-y-4">
-				<p className="text-base text-center">
-					Are you sure you want to delete{" "}
-					<span className="font-semibold">
-						{items.length} {correctTypeForm}
-					</span>
-					?
-				</p>
-				<div className="flex items-center justify-center gap-2">
-					<DangerButton type="button" className="rounded-lg">
-						Yes do it!
-					</DangerButton>
-					<SuccessButton type="button" className="rounded-lg">
-						No go back!
-					</SuccessButton>
-				</div>
-			</div>
-		</div>
+		<AnimatePresence>
+			{items.length && (
+				<motion.div
+					variants={variants}
+					initial="initial"
+					animate="animate"
+					exit="exit"
+					key="hello"
+					className="px-4 fixed bottom-4 max-w-[1008px] w-full"
+				>
+					<div className="bg-main p-4 border-white-100 border rounded-xl flex items-center justify-between w-full max-md:flex-col gap-y-4">
+						<p className="text-base text-center">
+							Are you sure you want to delete{" "}
+							<span className="font-semibold">
+								{items.length} {correctTypeForm}
+							</span>
+							?
+						</p>
+						<div className="flex items-center justify-center gap-2">
+							<DangerButton type="button" className="rounded-lg">
+								Yes do it!
+							</DangerButton>
+							<SuccessButton type="button" className="rounded-lg">
+								No go back!
+							</SuccessButton>
+						</div>
+					</div>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	);
 };
