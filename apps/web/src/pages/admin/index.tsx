bcf455fffae9b85863f97fce380d0272459e7f0a
ijsKoud <github@ijskoud.dev>
2023-01-16 18:12:41 +0100

    feat(AdminPanel): add admin statistics item

--- apps/web/src/pages/admin/index.tsx
+++ apps/web/src/pages/admin/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from "next";
-import { AdminLayout, DashboardStatistics, DashboardStorageUsage, Table, TableEntry } from "@paperplane/ui";
+import { AdminLayout, AdminStatistics, DashboardStorageUsage, Table, TableEntry } from "@paperplane/ui";
 
 const AdminPanel: NextPage = () => {
 	return (
 		<AdminLayout>
 			<div className="w-full h-80 flex gap-8 items-center px-2 max-md:flex-col max-md:h-auto">
 				<DashboardStorageUsage used={7.7e9} total={1e10} />
-				<DashboardStatistics files={1528} shorturls={38} />
+				<AdminStatistics users={13} auth="2FA" signupMode="closed" uptime={624_000} />
 			</div>
 			<div className="w-full px-2">
 				<div className="w-full rounded-lg bg-main p-8 flex flex-col gap-2">
