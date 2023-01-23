3aad54496b357d3908a6c4526dcda0fda787a3a1
ijsKoud <github@ijskoud.dev>
2023-01-23 19:19:48 +0100

    feat(Login): add admin login check

--- apps/server/src/api/auth/login.ts
+++ apps/server/src/api/auth/login.ts
@@ -1,16 +1,38 @@
 import type { Response, Request } from "express";
+import { Auth } from "../../lib/Auth.js";
 import type { RequestMethods } from "../../lib/types.js";
+import type Server from "../../Server.js";
 
-export default function handler(req: Request, res: Response) {
-	const { domain, code, password } = req.body ?? {};
-	if (typeof domain !== "string") {
+export default async function handler(server: Server, req: Request, res: Response) {
+	const { domain: _domain, code, password } = req.body ?? {};
+
+	if (typeof _domain !== "string") {
+		res.status(400).send({ message: "Invalid domain provided" });
+		return;
+	}
+
+	const domain = await server.prisma.domain.findFirst({ where: { domain: _domain, isSignUp: false } });
+	if (!domain && _domain !== "admin") {
 		res.status(400).send({ message: "Invalid domain provided" });
 		return;
 	}
 
 	if (code) {
 		if (typeof code !== "number" || code.toString().length !== 6) {
-			res.status(400).send({ message: "Invalid Two Factor Authentication provided" });
+			res.status(400).send({ message: "Invalid Two Factor Authentication code provided" });
+			return;
+		}
+
+		if (_domain === "admin") {
+			const authSecret = server._config.config.admin2FASecret;
+			const authRes = Auth.verify2FASecret(authSecret, code.toString());
+			if (!authRes || authRes.delta !== 0) {
+				res.status(400).send({ message: "Invalid Two Factor Authentication code provided" });
+				return;
+			}
+
+			res.cookie("PAPERPLANE-ADMIN", Auth.createJWTToken("admin", server._config.config.encryptionKey), { maxAge: 6.048e8 });
+			res.sendStatus(204);
 			return;
 		}
 
