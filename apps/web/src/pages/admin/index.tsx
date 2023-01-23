78cfca6862f5054560a7fca497579dd7ad9f7ace
ijsKoud <github@ijskoud.dev>
2023-01-23 20:26:32 +0100

    feat(AdminPanel): protect pages against signout users

--- apps/web/src/pages/admin/index.tsx
+++ apps/web/src/pages/admin/index.tsx
@@ -1,5 +1,25 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { AdminLayout, AdminStatistics, DashboardStorageUsage, Table, TableEntry } from "@paperplane/ui";
+import axios from "axios";
+import { getProtocol } from "@paperplane/utils";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const stateRes = await axios.get<{ admin: boolean; domains: string[] }>(`${getProtocol()}${context.req.headers.host}/api/auth/state`, {
+		headers: { "X-PAPERPLANE-ADMIN-KEY": context.req.cookies["PAPERPLANE-ADMIN"] }
+	});
+
+	if (!stateRes.data.admin)
+		return {
+			redirect: {
+				destination: "/login",
+				permanent: false
+			}
+		};
+
+	return {
+		props: {}
+	};
+};
 
 const AdminPanel: NextPage = () => {
 	return (
