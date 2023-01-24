d50232970fe3b6a0c88ccf7d36a6fa9181703ff1
ijsKoud <github@ijskoud.dev>
2023-01-24 20:12:24 +0100

    fix(SelectMenu): menuPlacement not applying

--- packages/forms/src/Select/SelectMenu.tsx
+++ packages/forms/src/Select/SelectMenu.tsx
@@ -1,7 +1,7 @@
 import type React from "react";
 import ReactSelect, { Props } from "react-select";
 import { getCleanProps } from "../utils";
-import { SelectInput, SelectControl, SelectMenuComponent, SelectOption, SelectDropdownIndicator, SelectSingleValue } from "./custom";
+import { SelectInput, SelectControl, SelectOption, SelectDropdownIndicator, SelectSingleValue } from "./custom";
 import type { SelectOption as SelectOptionType } from "./types";
 import { SelectMenuStyles } from "./utils";
 
@@ -21,13 +21,14 @@ export const SelectMenu: React.FC<SelectMenuProps<SelectOptionType>> = (props) =
 			components={{
 				Input: SelectInput,
 				Control: (_props) => <SelectControl {..._props} type={props.type} />,
-				Menu: (_props) => <SelectMenuComponent {..._props} type={props.type} />,
 				Option: (_props) => <SelectOption {..._props} style={props.type} />,
 				DropdownIndicator: (_props) => <SelectDropdownIndicator {..._props} type={props.type} />,
 				SingleValue: SelectSingleValue,
 				IndicatorSeparator: null
 			}}
 			classNames={{
+				menu: () => "!bg-transparent !shadow-none !rounded-xl",
+				menuList: () => `top-full absolute overflow-x-hidden w-full z-10 !rounded-lg shadow-md my-2 border ${styles.selectMenuComponent}`,
 				clearIndicator: () => "!text-white hover:!text-white-500",
 				placeholder: () => "!text-white-600",
 				multiValue: () => styles.multiValue,
