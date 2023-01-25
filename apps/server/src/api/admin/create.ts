2a9a41c22b3ac5beebab046590f1885c10028b97
ijsKoud <github@ijskoud.dev>
2023-01-25 10:57:39 +0100

    feat: add error handling

--- apps/server/src/api/admin/create.ts
+++ apps/server/src/api/admin/create.ts
@@ -23,47 +23,56 @@ export default async function handler(server: Server, req: Request, res: Respons
 	}
 
 	if (req.method === "POST") {
-		const data = req.body as Required<CreateUserFormBody>;
-		if (!data.domain || !domains.map((domain) => domain.domain).includes(data.domain)) {
-			res.status(400).send({ message: "Invalid sign-up domain provided" });
+		try {
+			const data = req.body as Required<CreateUserFormBody>;
+			if (!data.domain || !domains.map((domain) => domain.domain).includes(data.domain)) {
+				res.status(400).send({ message: "Invalid sign-up domain provided" });
+				return;
+			}
+
+			const DOMAIN_REGEX = /[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?/g;
+			const extensionRes = (data.extension ?? "").match(DOMAIN_REGEX) ?? [];
+			data.extension = extensionRes[0] ?? "";
+
+			if (data.domain.startsWith("*.") && !data.extension.length) {
+				res.status(400).send({ message: "Invalid sign-up subdomain provided" });
+				return;
+			}
+
+			if (!["block", "pass"].includes(data.extensionsMode)) {
+				res.status(400).send({ message: "Invalid extensionMode provided" });
+				return;
+			}
+
+			const auditlog = ms(data.auditlog);
+			if (isNaN(auditlog)) {
+				res.status(400).send({ message: "Invalid auditlog duration provided" });
+				return;
+			}
+
+			data.extensions = data.extensions.filter((ext) => ext.startsWith(".") && !ext.endsWith("."));
+			const domain = data.domain.startsWith("*.") ? `` : data.domain;
+
+			await server.domains.create({
+				disabled: false,
+				domain,
+				extensionsList: data.extensions.join(","),
+				extensionsMode: data.extensionsMode,
+				maxStorage: data.storage,
+				maxUploadSize: data.uploadSize,
+				auditlogDuration: data.auditlog
+			});
+
+			if (!data.domain.startsWith("*.")) {
+				await server.prisma.signupDomain.delete({ where: { domain: data.domain } });
+			}
+
+			res.sendStatus(204);
 			return;
+		} catch (err) {
+			server.logger.fatal(`[CREATE:POST]: Fatal error while creating a new PaperPlane account `, err);
+			res.status(500).send({ message: "Internal server error occured, please try again later." });
 		}
-
-		const DOMAIN_REGEX = /[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?/g;
-		const extensionRes = (data.extension ?? "").match(DOMAIN_REGEX) ?? [];
-		data.extension = extensionRes[0] ?? "";
-
-		if (data.domain.startsWith("*.") && !data.extension.length) {
-			res.status(400).send({ message: "Invalid sign-up subdomain provided" });
-			return;
-		}
-
-		if (!["block", "pass"].includes(data.extensionsMode)) {
-			res.status(400).send({ message: "Invalid extensionMode provided" });
-			return;
-		}
-
-		const auditlog = ms(data.auditlog);
-		if (isNaN(auditlog)) {
-			res.status(400).send({ message: "Invalid auditlog duration provided" });
-			return;
-		}
-
-		data.extensions = data.extensions.filter((ext) => ext.startsWith(".") && !ext.endsWith("."));
-		const domain = data.domain.startsWith("*.") ? `` : data.domain;
-
-		await server.domains.create({
-			disabled: false,
-			domain,
-			extensionsList: data.extensions.join(","),
-			extensionsMode: data.extensionsMode,
-			maxStorage: data.storage,
-			maxUploadSize: data.uploadSize,
-			auditlogDuration: data.auditlog
-		});
-
-		res.sendStatus(204);
-		return;
 	}
 
 	if (req.method === "PUT") {
