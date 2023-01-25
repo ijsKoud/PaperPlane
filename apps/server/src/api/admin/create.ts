a8baccfd6ae43bcf4ff007c1560d1fe83a51d3ab
ijsKoud <github@ijskoud.dev>
2023-01-25 10:41:01 +0100

    feat: add create post api

--- apps/server/src/api/admin/create.ts
+++ apps/server/src/api/admin/create.ts
@@ -1,12 +1,13 @@
 import type { Response, Request } from "express";
+import ms from "ms";
 import { Auth } from "../../lib/Auth.js";
-import type { Middleware, RequestMethods } from "../../lib/types.js";
+import type { CreateUserFormBody, Middleware, RequestMethods } from "../../lib/types.js";
 import type Server from "../../Server.js";
 
 export default async function handler(server: Server, req: Request, res: Response) {
-	if (req.method === "GET") {
-		const domains = await server.prisma.signupDomain.findMany();
+	const domains = await server.prisma.signupDomain.findMany();
 
+	if (req.method === "GET") {
 		res.send({
 			defaults: {
 				extensions: server.envConfig.extensionsList,
@@ -17,6 +18,56 @@ export default async function handler(server: Server, req: Request, res: Respons
 			},
 			domains: domains.map((domain) => domain.domain)
 		});
+
+		return;
+	}
+
+	if (req.method === "POST") {
+		const data = req.body as Required<CreateUserFormBody>;
+		if (!data.domain || !domains.map((domain) => domain.domain).includes(data.domain)) {
+			res.status(400).send({ message: "Invalid sign-up domain provided" });
+			return;
+		}
+
+		const DOMAIN_REGEX = /[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?/g;
+		const extensionRes = (data.extension ?? "").match(DOMAIN_REGEX) ?? [];
+		data.extension = extensionRes[0] ?? "";
+
+		if (data.domain.startsWith("*.") && !data.extension.length) {
+			res.status(400).send({ message: "Invalid sign-up subdomain provided" });
+			return;
+		}
+
+		if (!["block", "pass"].includes(data.extensionsMode)) {
+			res.status(400).send({ message: "Invalid extensionMode provided" });
+			return;
+		}
+
+		const auditlog = ms(data.auditlog);
+		if (isNaN(auditlog)) {
+			res.status(400).send({ message: "Invalid auditlog duration provided" });
+			return;
+		}
+
+		data.extensions = data.extensions.filter((ext) => ext.startsWith(".") && !ext.endsWith("."));
+		const domain = data.domain.startsWith("*.") ? `` : data.domain;
+
+		await server.domains.create({
+			disabled: false,
+			domain,
+			extensionsList: data.extensions.join(","),
+			extensionsMode: data.extensionsMode,
+			maxStorage: data.storage,
+			maxUploadSize: data.uploadSize,
+			auditlogDuration: data.auditlog
+		});
+
+		res.sendStatus(204);
+		return;
+	}
+
+	if (req.method === "PUT") {
+		// TODO: Add PUT method
 	}
 }
 
