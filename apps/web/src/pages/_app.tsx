6b30fa616922ff285d0da89dd52683ab112efb10
ijsKoud <github@ijskoud.dev>
2023-01-29 17:34:24 +0100

    feat: add Next SEO

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,16 +3,20 @@ import "../styles/output.css";
 import "../styles/fontawesome.css";
 import "react-toastify/dist/ReactToastify.min.css";
 
+import SEO from "../../next-seo.config";
+
 import type { AppProps } from "next/app";
 import { Poppins } from "@next/font/google";
 import { SwrWrapper } from "@paperplane/swr";
 import { ToastContainer } from "react-toastify";
+import { DefaultSeo } from "next-seo";
 
 const poppins = Poppins({ weight: ["300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"], display: "swap" });
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<SwrWrapper>
+			<DefaultSeo {...SEO} />
 			<ToastContainer position="top-right" theme="dark" bodyStyle={poppins.style} />
 			<main className={`bg-bg-dark min-h-screen min-w-full ${poppins.className}`}>
 				<Component {...pageProps} />
