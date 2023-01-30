9f9de736c01a3262554f8e9536925ab11e7c6eac
ijsKoud <github@ijskoud.dev>
2023-01-30 15:44:47 +0100

    feat(State): add domain checking

--- apps/server/src/api/auth/state.ts
+++ apps/server/src/api/auth/state.ts
@@ -8,8 +8,16 @@ export default function handler(server: Server, req: Request, res: Response) {
 	const adminAuthSecret = Array.isArray(adminAuthHeader) ? adminAuthHeader[0] : adminAuthHeader ?? "";
 	const admin = adminAuthSecret ? Auth.verifyJWTToken(adminAuthSecret, server.envConfig.encryptionKey, "admin") : false;
 
+	const proxyHost = req.headers["x-forwarded-host"];
+	const hostName = proxyHost ? proxyHost : req.headers.host ?? req.hostname;
+
+	const host = server.domains.domains.find((dm) => dm.domain.startsWith(Array.isArray(hostName) ? hostName[0] : hostName));
+	const domainAuthHeader = req.headers["x-paperplane-auth-key"];
+	const domainAuthSecret = Array.isArray(domainAuthHeader) ? domainAuthHeader[0] : domainAuthHeader ?? "";
+	const domain = domainAuthSecret ? Auth.verifyJWTToken(domainAuthSecret, server.envConfig.encryptionKey, host?.domain || req.hostname) : false;
+
 	res.send({
-		domain: false,
+		domain,
 		admin
 	});
 }
