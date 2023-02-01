60ef1a4aa0cff7e30a3058a65e0a183842edfc9e
ijsKoud <github@ijskoud.dev>
2023-02-01 16:48:46 +0100

    feat(Stats): add real-time files and urls count

--- apps/server/src/api/dashboard/stats.ts
+++ apps/server/src/api/dashboard/stats.ts
@@ -3,15 +3,18 @@ import { Auth } from "../../lib/Auth.js";
 import type { DashboardRequest, Middleware, RequestMethods } from "../../lib/types.js";
 import type Server from "../../Server.js";
 
-export default function handler(server: Server, req: DashboardRequest, res: Response) {
+export default async function handler(server: Server, req: DashboardRequest, res: Response) {
 	if (!req.locals.domain) {
 		res.status(500).send({ message: "Internal server error, please try again later." });
 		return;
 	}
 
+	const files = await server.prisma.file.count({ where: { domain: req.locals.domain.domain } });
+	const shorturls = await server.prisma.url.count({ where: { domain: req.locals.domain.domain } });
+
 	res.send({
-		files: 0,
-		shorturls: 0,
+		files,
+		shorturls,
 		storage: {
 			total: req.locals.domain.maxStorage,
 			used: req.locals.domain.storage
