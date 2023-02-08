489b066a9082935e404c8970ecf02b34f8f3262d
ijsKoud <github@ijskoud.dev>
2023-02-08 09:44:50 +0100

    build: prisma db migration error

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,9 @@
 import { config } from "dotenv";
-import { exec } from "node:child_process";
 import { join } from "node:path";
 config({ path: join(process.cwd(), "..", "..", "data", ".env") });
 
 import Server from "./Server.js";
 void (async () => {
-	await new Promise((res, rej) => exec("prisma db push", (err) => (err ? rej(err) : res(null))));
 	const server = new Server();
 	await server.run();
 })();
