eae1d706975896d65e6f22bff9a1374242928119
Daan Klarenbeek <github@ijskoud.dev>
2023-04-26 16:47:29 +0200

    fix: still not working

--- apps/web/next.config.cjs
+++ apps/web/next.config.cjs
@@ -1,21 +1,11 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-var-requires */
-// const { readdirSync } = require("node:fs");
-// const { join } = require("node:path");
-// const packages = readdirSync(join(process.cwd(), "..", "..", "packages"));
+const { readdirSync } = require("node:fs");
+const { join } = require("node:path");
+const packages = readdirSync(join(process.cwd(), "..", "..", "packages"));
 
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
-	transpilePackages: [
-		"@paperplane/buttons",
-		"@paperplane/forms",
-		"@paperplane/logo",
-		"@paperplane/markdown",
-		"@paperplane/modal",
-		"@paperplane/navbar",
-		"@paperplane/swr",
-		"@paperplane/ui",
-		"@paperplane/utils"
-	]
+	transpilePackages: packages.map((str) => `@paperplane/${str}`)
 };
