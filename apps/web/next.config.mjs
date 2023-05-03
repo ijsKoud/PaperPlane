4f05a345a9c01901fbf3eeaca586649af9314719
Daan Klarenbeek <github@ijskoud.dev>
2023-05-03 13:46:41 +0200

    fix: nextjs not starting due to dynamic list

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,22 @@
-import { readdirSync } from "node:fs";
-import { join } from "node:path";
+// import { readdirSync } from "node:fs";
+// import { join } from "node:path";
 
-const transpilePackages = readdirSync(join(process.cwd(), "..", "..", "packages")).map((pkg) => `@paperplane/${pkg}`);
+// const transpilePackages = readdirSync(join(process.cwd(), "..", "..", "packages")).map((pkg) => `@paperplane/${pkg}`);
 
 /** @type {import('next').NextConfig} */
 const config = {
 	reactStrictMode: false,
-	transpilePackages
+	transpilePackages: [
+		"@paperplane/buttons",
+		"@paperplane/forms",
+		"@paperplane/logo",
+		"@paperplane/markdown",
+		"@paperplane/modal",
+		"@paperplane/navbar",
+		"@paperplane/swr",
+		"@paperplane/ui",
+		"@paperplane/utils"
+	]
 };
 
 export default config;
