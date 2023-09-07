e00f9a5f7b7699cc77a7a66931e5ee68d23aaa1d
Daan Klarenbeek <github@ijskoud.dev>
2023-09-07 22:25:58 +0200

    feat(DashboardSettings): add settings form

---
+++ apps/web/src/app/dashboard/settings/Settings.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useSwrWithUpdates } from "@paperplane/swr";
+import { DashboardSettingsGetApi } from "@paperplane/utils";
+import React, { useEffect, useState } from "react";
+import ApiTokenSettings from "./_sections/ApiTokenSettings";
+import SettingsForm from "./_sections/Form";
+
+const UseDashboardSettings = () => {
+	const [settings, setSettings] = useState<DashboardSettingsGetApi>({
+		embedEnabled: false,
+		nameLength: 10,
+		nameStrategy: "id",
+		tokens: []
+	});
+	const { data } = useSwrWithUpdates<DashboardSettingsGetApi>("/api/dashboard/settings");
+
+	useEffect(() => {
+		if (data) setSettings(data);
+	}, [data]);
+
+	return settings;
+};
+
+const Settings: React.FC = () => {
+	const settings = UseDashboardSettings();
+
+	return (
+		<div className="space-y-20">
+			<ApiTokenSettings tokens={settings.tokens} />
+			<SettingsForm {...settings} />
+		</div>
+	);
+};
+
+export default Settings;
