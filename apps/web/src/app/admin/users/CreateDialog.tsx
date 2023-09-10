3fd13642bd8369c28c69ec88c320304cd3173cec
Daan Klarenbeek <github@ijskoud.dev>
2023-09-10 16:07:17 +0200

    feat(AdminUserPanel): add create dialog

---
+++ apps/web/src/app/admin/users/CreateDialog.tsx
@@ -0,0 +1,358 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@paperplane/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@paperplane/ui/dialog";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage, FormDescription } from "@paperplane/ui/form";
+import { Input } from "@paperplane/ui/input";
+import { Loader2, PlusCircleIcon, UserIcon } from "lucide-react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import axios, { AxiosError } from "axios";
+import { useToast } from "@paperplane/ui/use-toast";
+import { CreateGetApi, STORAGE_UNITS, TIME_UNITS_ARRAY, formatBytes, parseToDay } from "@paperplane/utils";
+import ms from "ms";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@paperplane/ui/select";
+import { ScrollArea } from "@paperplane/ui/scroll-area";
+import { useSwr } from "@paperplane/swr";
+
+const getStorage = (storage: number): string[] => {
+	const res = formatBytes(storage);
+	return res.split(/ +/g);
+};
+
+export const CreateDialog: React.FC = () => {
+	const { toast } = useToast();
+	const [extensionDisabled, setExtensionDisabled] = useState(true);
+	const { data: createGetData } = useSwr<CreateGetApi>("/api/admin/create", undefined, (url) =>
+		axios.get(url, { withCredentials: true }).then((res) => res.data)
+	);
+
+	const FormSchema = z.object({
+		storage: z.number({ required_error: "A max storage is required" }).min(0, "Storage cannot be below 0 K.B"),
+		storageUnit: z.string().refine((arg) => STORAGE_UNITS.includes(arg as any), "The provided unit is not valid"),
+		uploadSize: z.number({ required_error: "An upload size is required" }).min(0, "Upload size cannot be below 0 K.B"),
+		uploadSizeUnit: z.string().refine((arg) => STORAGE_UNITS.includes(arg as any), "The provided unit is not valid"),
+		extensions: z.array(z.string()),
+		extensionsMode: z.union([z.literal("pass"), z.literal("block")], {
+			invalid_type_error: "The provided mode is not valid",
+			required_error: "An extension mode is required"
+		}),
+		auditlog: z.number({ required_error: "An auditlog duration is required" }).min(0, "Auditlog duration cannot be below 0 seconds"),
+		auditlogUnit: z.string().refine((arg) => TIME_UNITS_ARRAY.includes(arg as any), "The provided unit is not valid"),
+		domain: z.string({ required_error: "The domain is a required option" }).refine((arg) => createGetData?.domains.includes(arg)),
+		extension: z.string()
+	});
+
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+		values: {
+			extensions: createGetData?.defaults.extensions ?? [],
+			extensionsMode: createGetData?.defaults.extensionsMode || "pass",
+			storage: Number(getStorage(createGetData?.defaults.maxStorage ?? 0)[0]),
+			storageUnit: getStorage(createGetData?.defaults.maxStorage ?? 0)[1] as any,
+			uploadSize: Number(getStorage(createGetData?.defaults.maxUploadSize ?? 0)[0]),
+			uploadSizeUnit: getStorage(createGetData?.defaults.maxUploadSize ?? 0)[1] as any,
+			auditlog: Number(
+				ms(createGetData?.defaults.auditlog ?? 0)
+					.split("")
+					.filter((str) => !isNaN(Number(str)))
+					.join("")
+			),
+			auditlogUnit: ms(createGetData?.defaults.auditlog ?? 0)
+				.split("")
+				.filter((str) => isNaN(Number(str)))
+				.join(""),
+			domain: "",
+			extension: ""
+		}
+	});
+
+	async function onSubmit(data: z.infer<typeof FormSchema>) {
+		try {
+			await axios.post(
+				"/api/admin/create",
+				{
+					domain: data.domain,
+					extension: data.extension,
+					extensions: data.extensions,
+					extensionsMode: data.extensionsMode,
+					auditlog: parseToDay(data.auditlog, data.auditlogUnit as any),
+					storage: `${data.storage} ${data.storageUnit}`,
+					uploadSize: `${data.uploadSize} ${data.uploadSizeUnit}`
+				},
+				{ withCredentials: true }
+			);
+			toast({ title: "User created", description: "A new user has been created." });
+		} catch (err) {
+			const error = "isAxiosError" in err ? (err as AxiosError<{ message: string }>).response?.data.message || "n/a" : "n/a";
+			toast({ variant: "destructive", title: "Uh oh! Something went wrong", description: `There was a problem with your request: ${error}` });
+			form.setFocus("domain");
+			console.log(err);
+		}
+	}
+
+	const onDomainChange = (value: string, onChange: (...values: any[]) => void) => {
+		onChange(value);
+		setExtensionDisabled(!value.startsWith("*."));
+	};
+
+	return (
+		<Dialog onOpenChange={() => form.reset()}>
+			<DialogTrigger asChild>
+				<Button variant="outline">
+					<UserIcon className="mr-2 w-4 h-4" />
+					Create User
+				</Button>
+			</DialogTrigger>
+
+			<DialogContent>
+				<ScrollArea className="max-h-[calc(100vh-128px)] pr-4">
+					<DialogHeader>
+						<DialogTitle>Create new user</DialogTitle>
+						<DialogDescription>
+							Creating an account as admin is always possible, keep in mind that you cannot set a password or get access to the 2FA
+							code. The user will have to use the default back-up code <strong>paperplane-cdn</strong> to login.
+						</DialogDescription>
+					</DialogHeader>
+
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 mt-2">
+							<div className="flex items-center gap-2">
+								<FormField
+									control={form.control}
+									name="extension"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel className="text-base">Domain</FormLabel>
+											<FormControl>
+												<Input
+													disabled={extensionDisabled}
+													value={field.value}
+													onChange={field.onChange}
+													placeholder="Domain extension here..."
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="domain"
+									render={({ field }) => (
+										<FormItem className="mt-auto w-3/4">
+											<Select required value={field.value} onValueChange={(value) => onDomainChange(value, field.onChange)}>
+												<FormControl>
+													<SelectTrigger className="w-full">
+														<SelectValue placeholder="Select a valid unit" />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent className="overflow-y-auto max-h-56">
+													{createGetData?.domains.map((domain, key) => (
+														<SelectItem key={key} value={domain}>
+															{domain}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+
+							<div className="flex items-center gap-2">
+								<FormField
+									control={form.control}
+									name="storage"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel className="text-base">Max Storage</FormLabel>
+											<FormDescription>
+												This allows you to set the Upload Size for PaperPlane accounts when they are created. Note: if you use
+												a reverse-proxy like NGINX, make sure to configure an upload limit there too, otherwise this will not
+												work.
+											</FormDescription>
+											<FormControl>
+												<Input value={field.value} onChange={field.onChange} placeholder="Valid amount here..." />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="storageUnit"
+									render={({ field }) => (
+										<FormItem className="mt-auto">
+											<Select required value={field.value} onValueChange={field.onChange}>
+												<FormControl>
+													<SelectTrigger className="w-full">
+														<SelectValue placeholder="Select a valid unit" />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent className="overflow-y-auto max-h-56">
+													{STORAGE_UNITS.map((unit, key) => (
+														<SelectItem key={key} value={unit}>
+															{unit}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+
+							<div className="flex items-center gap-2">
+								<FormField
+									control={form.control}
+									name="uploadSize"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel className="text-base">Upload Size</FormLabel>
+											<FormDescription>
+												This allows you to set the Upload Size for PaperPlane accounts when they are created. Note: if you use
+												a reverse-proxy like NGINX, make sure to configure an upload limit there too, otherwise this will not
+												work.
+											</FormDescription>
+											<FormControl>
+												<Input value={field.value} onChange={field.onChange} placeholder="Valid amount here..." />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="uploadSizeUnit"
+									render={({ field }) => (
+										<FormItem className="mt-auto">
+											<Select required value={field.value} onValueChange={field.onChange}>
+												<FormControl>
+													<SelectTrigger className="w-full">
+														<SelectValue placeholder="Select a valid unit" />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent className="overflow-y-auto max-h-56">
+													{STORAGE_UNITS.map((unit, key) => (
+														<SelectItem key={key} value={unit}>
+															{unit}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+
+							<div className="flex items-center gap-2">
+								<FormField
+									control={form.control}
+									name="auditlog"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel className="text-base">Auditlog Duration</FormLabel>
+											<FormDescription>
+												This will determine the for how long the audit logs are visible. By default it is set to 1 month.
+											</FormDescription>
+											<FormControl>
+												<Input value={field.value} onChange={field.onChange} placeholder="Valid amount here..." />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="auditlogUnit"
+									render={({ field }) => (
+										<FormItem className="mt-auto">
+											<Select required value={field.value} onValueChange={field.onChange}>
+												<FormControl>
+													<SelectTrigger className="w-full">
+														<SelectValue placeholder="Select a valid unit" />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent className="overflow-y-auto max-h-56">
+													{TIME_UNITS_ARRAY.map((unit, key) => (
+														<SelectItem key={key} value={unit}>
+															{unit}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+
+							<div className="flex items-center gap-2">
+								<FormField
+									control={form.control}
+									name="extensions"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel className="text-base">(Dis)Allowed Extensions</FormLabel>
+											<FormDescription>
+												This will allow you to accept/block the upload of certain file extensions (e.x. .png). Please use the
+												following format when using this:{" "}
+												<strong>{".<extension>,.<extension>,...etc (e.x.: .png,.jpg)."}</strong>
+											</FormDescription>
+											<FormControl>
+												<Input
+													value={field.value.join(",")}
+													onChange={(ctx) => field.onChange(ctx.currentTarget.value.split(","))}
+													placeholder="Valid amount here..."
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="extensionsMode"
+									render={({ field }) => (
+										<FormItem className="mt-auto">
+											<Select required value={field.value} onValueChange={field.onChange}>
+												<FormControl>
+													<SelectTrigger className="w-full">
+														<SelectValue placeholder="Select a valid unit" />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent className="overflow-y-auto max-h-56">
+													{["block", "pass"].map((unit, key) => (
+														<SelectItem key={key} value={unit}>
+															{unit}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+
+							<Button type="submit" className="w-full" disabled={form.formState.isSubmitting || !form.formState.isValid}>
+								{form.formState.isSubmitting ? (
+									<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+								) : (
+									<PlusCircleIcon className="mr-2 h-4 w-4" />
+								)}{" "}
+								Create user
+							</Button>
+						</form>
+					</Form>
+				</ScrollArea>
+			</DialogContent>
+		</Dialog>
+	);
+};
