cf2de30cb0fc0965a911cce4c26bf4c7d8445507
Daan Klarenbeek <github@ijskoud.dev>
2023-09-11 16:06:48 +0200

    chore: update branding

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,13 +15,13 @@ const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
 				<link rel="manifest" href="/favicons/site.webmanifest" />
-				<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#1f2021" />
+				<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#343434" />
 				<link rel="shortcut icon" href="/favicons/favicon.ico" />
 				<meta name="apple-mobile-web-app-title" content="PaperPlane" />
 				<meta name="application-name" content="PaperPlane" />
-				<meta name="msapplication-TileColor" content="#1f2021" />
+				<meta name="msapplication-TileColor" content="#da532c" />
 				<meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-				<meta name="theme-color" content="#1f2021" />
+				<meta name="theme-color" content="#343434" />
 			</head>
 			<body className="dark:bg-bg-dark bg-white" style={InterFont.style}>
 				<Providers>{children}</Providers>
