82b00d26558490ea8d131c6feb337f342ba67bdc
Daan Klarenbeek <github@ijskoud.dev>
2023-09-22 14:57:33 +0200

    chore: remove outdated code

--- apps/server/src/lib/index.ts
+++ apps/server/src/lib/index.ts
@@ -1,6 +1,5 @@
 export * from "./Auth.js";
 export * from "./types.js";
-export * from "./Api.js";
 export * from "./utils.js";
 export * from "./backups/index.js";
 export * from "./ChunkUpload.js";
