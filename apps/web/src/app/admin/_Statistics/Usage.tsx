474d452ba088a9e0d1583024ded96ea5514507a7
Daan Klarenbeek <github@ijskoud.dev>
2023-10-09 10:34:24 +0200

    fix(Usage): double % in text

--- apps/web/src/app/admin/_Statistics/Usage.tsx
+++ apps/web/src/app/admin/_Statistics/Usage.tsx
@@ -26,7 +26,7 @@ export const Usage: React.FC<ServiceProps> = ({ storageUsage, cpuUsage, memoryTo
 			<div className="dark:bg-zinc-900 bg-zinc-100 border border-zinc-200 dark:border-zinc-700 rounded-xl flex flex-col justify-center items-center min-w-[16rem] px-2 py-5 max-md:w-full">
 				<h1 className="text-5 font-medium">Cpu Usage</h1>
 				<p className="text-4 font-light">
-					<strong className="font-semibold">{cpuUsage}%</strong> in use
+					<strong className="font-semibold">{cpuUsage}</strong> in use
 				</p>
 			</div>
 			<div className="dark:bg-zinc-900 bg-zinc-100 border border-zinc-200 dark:border-zinc-700 rounded-xl flex flex-col justify-center items-center min-w-[16rem] px-2 py-5 max-md:w-full">
