802e24bbb6c66ca15cde92e1103db6c454e0fd71
Daan Klarenbeek <github@ijskoud.dev>
2023-10-16 17:01:32 +0200

    feat(UploadApi): add chunk completion api route

---
+++ apps/server/src/routes/api/v1/upload/chunk/complete.ts
@@ -0,0 +1,57 @@
+import type Domain from "#components/Domain.js";
+import { Utils } from "#lib/utils.js";
+import type Server from "#server.js";
+import { ApplyOptions, Route, methods } from "@snowcrystals/highway";
+import type { NextFunction, Request, Response } from "express";
+import { ZodError, z } from "zod";
+
+@ApplyOptions<Route.Options>({ ratelimit: { max: 25, windowMs: 1e3 }, middleware: [[methods.GET, "user-api-key"]] })
+export default class ApiRoute extends Route<Server> {
+	public [methods.GET](req: Request, res: Response, next: NextFunction, { domain }: Record<"domain", Domain>) {
+		const body = this.parseBody(req.body);
+		if (body instanceof ZodError) {
+			const errors = Utils.parseZodError(body);
+			res.status(400).send({ errors });
+			return;
+		}
+
+		const partialFileHandler = domain.partialFileManager.partials.get(body.id);
+		if (!partialFileHandler) {
+			res.status(409).send({
+				errors: [{ field: "id", code: "NOT_FOUND", message: "A partialfile handler with the provided id does not exist" }]
+			});
+			return;
+		}
+
+		if (partialFileHandler.status === "OPEN") {
+			partialFileHandler.complete();
+			res.status(200).send({ status: "PROCESSING", url: null });
+			return;
+		} else if (partialFileHandler.status === "PROCESSING") {
+			res.status(200).send({ status: "PROCESSING", url: null });
+			return;
+		}
+
+		clearTimeout(partialFileHandler.timeout);
+		domain.partialFileManager.partials.delete(body.id);
+
+		res.status(200).send({ status: "FINISHED", url: `${req.protocol}://${domain.domain}/files/${partialFileHandler.documentId}` });
+	}
+
+	/**
+	 * Parses the request body
+	 * @param body The body to parse
+	 * @returns Parsed json content or ZodError if there was a parsing issue
+	 */
+	private parseBody(body: any) {
+		const schema = z.object({
+			id: z.string({ invalid_type_error: "Property 'filename' must be a string", required_error: "Partialfile handler id is required" })
+		});
+
+		try {
+			return schema.parse(body);
+		} catch (error) {
+			return error as ZodError;
+		}
+	}
+}
